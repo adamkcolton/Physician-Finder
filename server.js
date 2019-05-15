bd30025c25940fa6451e6b74641c9d529d6f670e
adamkcolton <adamcolton216@gmail.com>
2019-05-14 21:04:15 -0700

    set up db find function

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require("express");
-// var mongojs = require("mongojs");
+var mongojs = require("mongojs");
 var bodyParser = require('body-parser');
 var app = express();
 var cors = require('cors');
@@ -14,20 +14,31 @@ app.use(function (req, res, next) {
     next();
 });
 
-// var databaseUrl = "physicianFinder_DB";
-// var collections = ["physicianData", "coords"];
+var databaseUrl = "physicianFinder_DB";
+var collections = ["physicianData", "coords"];
 
-// var db = mongojs(databaseUrl, collections);
-// db.on("error", function (error) {
-//     console.log("Database Error:", error);
-// });
+var db = mongojs(databaseUrl, collections);
+db.on("error", function (error) {
+    console.log("Database Error:", error);
+});
 
 //------------ROUTES------------//
 
 app.post('/search-physician/:first/:middle/:last', (req, res) => {
-    console.log(`${req.params.first} ${req.params.middle} ${req.params.last}`)
-    res.send(req.params.name)
-})
+    var firstName = req.params.first;
+    var middle = req.params.middle;
+    var lastName = req.params.last;
+    console.log(`${firstName} ${middle} ${lastName}`)
+
+    db.physicianData.find({ 'firstName': firstName, 'middle': middle, 'last': lastName }, function (error, found) {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            res.json(found)
+        }
+    });
+});
 
 
 
